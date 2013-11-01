77d38a89d05870f7d188872325e6637fc92ffb6d
RobsGitty <rob.vanoeveren@gmail.com>
2013-11-01 19:03:25 +0100

    commit proj2

---
+++ prj2testgithub/src/prj2testgithub/Prj2testgithub.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package prj2testgithub;
+
+/**
+ *
+ * @author Rob
+ */
+public class Prj2testgithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Ja en dit wordt prj2testgithub\n");
+    }
+    
+}
